f2789dbacf79ebfd193aaf69af7a6a6b74e9aa38
pratik-6958 <kolhapuri9075@gmail.com>
2026-01-03 20:20:05 +0530

    Update main.tf
    ghj

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
 resource "aws_instance" "demo_ec2" {
   ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1
   instance_type = "t2.micro"
