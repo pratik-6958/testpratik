4a5ab2129d5a16557ee9c83a5b815e985ed579f7
pratik-6958 <kolhapuri9075@gmail.com>
2026-01-03 20:15:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo_ec2" {
+  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-demo-ec2"
+  }
+}
